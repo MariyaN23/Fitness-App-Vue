e33262e5c824ecbfdcd71f66325868f963af8d04
MariyaN23 <138151774+MariyaN23@users.noreply.github.com>
2025-07-21 17:14:57 +0300

    preparing before dev

---
+++ src/types/workout.types.ts
@@ -0,0 +1,19 @@
+export type IWorkoutProgram = {
+    [key: number]: WorkoutDay
+}
+type WorkoutDay = {
+    warmup: Exercise[]
+    workout: Exercise[]
+}
+
+type Exercise = {
+    name: string
+    sets: number
+    reps: number
+}
+
+export type IExerciseDescriptions = {
+    [exerciseName: string]: string
+}
+
+export type IGymHealthFacts = string[]

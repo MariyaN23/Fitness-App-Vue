a080a26b2350b2c95d829b6a8008966eeafe2b5e
MariyaN23 <138151774+MariyaN23@users.noreply.github.com>
2025-07-24 17:20:15 +0300

    created layout and welcome page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts">
+import Welcome from "./components/pages/Welcome.vue";
+import Layout from "./components/layouts/Layout.vue";
+import Dashboard from "./components/pages/Dashboard.vue";
 </script>
 
 <template>
+  <Layout>
+    <Welcome/>
+    <Dashboard/>
+  </Layout>
 </template>
 
 <style scoped>
--- src/components/layouts/Layout.vue
+++ src/components/layouts/Layout.vue
@@ -2,7 +2,63 @@
 </script>
 
 <template>
+  <header>
+    <h1 class="text-gradient">SMOLGRAM</h1>
+  </header>
+  <main>
+    <slot/>
+  </main>
+  <footer>
+    <small>Created by</small>
+    <a href="https://github.com/MariyaN23" target="_blank">
+      <img src="https://avatars.githubusercontent.com/u/138151774?v=4" alt="pfp"/>
+      <p>MariyaN23</p>
+    </a>
+  </footer>
 </template>
 
 <style scoped>
+header,
+main,
+footer {
+  padding: 1rem;
+  width: 100%;
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+main {
+  flex: 1;
+}
+
+footer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5rem;
+  padding: 2rem 0 3rem;
+}
+
+footer a {
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  padding: 0.3rem 0.5rem 0.3rem 0.3rem;
+  background: var(--background-muted);
+  border: 1px solid var(--color-link-transparent);
+  border-radius: 4rem;
+  transition-duration: 200ms;
+  text-decoration: none;
+}
+
+footer a:hover {
+  border-color: var(--color-link);
+}
+
+footer a img {
+  max-width: 30px;
+  aspect-ratio: 1/1;
+  border-radius: 100%;
+}
+
 </style>

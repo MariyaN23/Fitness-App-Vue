8b6658b9dd074f56db6763de71de8b9f49ca9528
MariyaN23 <138151774+MariyaN23@users.noreply.github.com>
2025-07-28 17:31:14 +0300

    created workout page template

--- src/components/pages/Workout.vue
+++ src/components/pages/Workout.vue
@@ -1,8 +1,138 @@
 <script setup lang="ts">
+import { workoutProgram } from "../../utils";
+
+const selectedWorkouts = 4
+const {workout, warmup} = workoutProgram[selectedWorkouts]
 </script>
 
 <template>
+  <section id="workout-card">
+    <div class="plan-card card">
+      <div class="plan-card-header">
+        <p>Day {{ selectedWorkouts < 9 ? '0' + selectedWorkouts : selectedWorkouts }}</p>
+        <i class="fa-solid fa-dumbbell"></i>
+      </div>
+      <h2>{{ 'Push' }} Workout</h2>
+    </div>
+    <div class="workout-grid">
+      <h4 class="grid-name">Warmup</h4>
+      <h6>Sets</h6>
+      <h6>Reps</h6>
+      <h6 class="grid-weights">Weights</h6>
+      <div class="workout-grid-row" v-for="(w, wIdx) in warmup" :key="wIdx">
+        <div class="grid-name">
+          <p>{{ w.name }}</p>
+          <button>
+            <i class="fa-regular fa-circle-question"></i>
+          </button>
+        </div>
+        <p>{{ w.sets }}</p>
+        <p>{{ w.reps }}</p>
+        <input class="grid-weights" placeholder="14kg" type="text" disabled />
+      </div>
+      <div class="workout-grid-line"></div>
+      <h4 class="grid-name">Workout</h4>
+      <h6>Sets</h6>
+      <h6>Reps</h6>
+      <h6 class="grid-weights">Weights</h6>
+      <div class="workout-grid-row" v-for="(w, wIdx) in workout" :key="wIdx">
+        <div class="grid-name">
+          <p>{{ w.name }}</p>
+          <button>
+            <i class="fa-regular fa-circle-question"></i>
+          </button>
+        </div>
+        <p>{{ w.sets }}</p>
+        <p>{{ w.reps }}</p>
+        <input class="grid-weights" placeholder="14kg" type="text" />
+      </div>
+    </div>
+    <div class="workout-buttons card">
+      <button>
+        Save & Exit
+        <i class="fa-solid fa-save"></i>
+      </button>
+      <button>
+        Complete
+        <i class="fa-solid fa-check"></i>
+      </button>
+    </div>
+  </section>
 </template>
 
 <style scoped>
+#workout-card,
+.plan-card {
+  display: flex;
+  flex-direction: column;
+}
+
+#workout-card {
+  gap: 1.5rem;
+}
+
+.plan-card-header,
+.workout-buttons {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 1rem;
+}
+
+.workout-grid,
+.workout-grid-row {
+  display: grid;
+  grid-template-columns: repeat(7, minmax(0, 1fr));
+  gap: 1rem;
+}
+
+.workout-grid-row,
+.workout-grid-line {
+  grid-column: span 7 / span 7;
+}
+
+.grid-name {
+  grid-column: span 3 / span 3;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+}
+
+.grid-name button {
+  padding: 0;
+  border: none;
+  box-shadow: none;
+}
+
+.grid-name button:hover {
+  transform: none;
+  box-shadow: none;
+  color: var(--color-link);
+}
+
+.workout-grid-row .grid-name button {
+  opacity: 0;
+  pointer-events: none;
+}
+
+.workout-grid-row:hover .grid-name button {
+  opacity: 1;
+  pointer-events: all;
+}
+
+.grid-name p {
+  text-transform: capitalize;
+}
+
+.grid-weights {
+  grid-column: span 2 / span 2;
+}
+
+.workout-buttons button {
+  flex: 1;
+}
+
+.workout-buttons button i {
+  padding-left: 0.5rem;
+}
 </style>
